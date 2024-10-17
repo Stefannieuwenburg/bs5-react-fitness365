8a2f79030df14de911de0da69083cd33dccdf656
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2024-10-17 23:36:08 +0200

    footer update commit

--- src/Contact.js
+++ src/Contact.js
@@ -6,13 +6,13 @@ const Contact = () => {
   return (
     <div className=" " id="Contact">
       <footer id="Contact">
-        <Container>
-          <ListGroup className="my-4 bg-dark rounded">
+              <ListGroup className="m-4 bg-dark rounded">
+                  <Container>
             <ListGroup.Item> Aantal weken:4 </ListGroup.Item>
             <ListGroup.Item> Tel: 085-4871920 </ListGroup.Item>
-            <ListGroup.Item > fittness365 </ListGroup.Item>
+                  <ListGroup.Item > fittness365 </ListGroup.Item>
+                  </Container>
           </ListGroup>
-        </Container>
       </footer>
     </div>
   );
